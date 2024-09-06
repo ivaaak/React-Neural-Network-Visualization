7d73b8d0e99871d6eccd88423e1be62b66bd9c9a
Ivaylo Pavlov <43663336+ivaaak@users.noreply.github.com>
2024-09-06 18:05:48 +0300

    feat: boilerplate code

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Animated from './Animated'
+
+function App() {
+  return (
+    <div className=".bg-gray-900">
+      <h1> Animated Neural Network </h1>
+      
+      <Animated></Animated>
+    </div>
+  )
+}
+
+export default App
